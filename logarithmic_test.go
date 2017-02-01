8027d40fdcf38551191c8a0296a39a38d9d46f65
Dominique d’Argent <dominique.dargent@me.com>
2017-02-01 11:31:36 +0100

    Refactor tests

--- logarithmic_test.go
+++ logarithmic_test.go
@@ -4,26 +4,11 @@ import "testing"
 
 func TestLogarithmic(t *testing.T) {
 	r := NewLogarithmic()
-	err := r.Fit(
-		dataPoint{x: 6, y: 30},
-		dataPoint{x: 20, y: 12},
-		dataPoint{x: 26, y: 9},
-		dataPoint{x: 30, y: 8},
-		dataPoint{x: 37, y: 6},
-		dataPoint{x: 42, y: 5},
-		dataPoint{x: 47, y: 4},
-		dataPoint{x: 54, y: 3},
-		dataPoint{x: 64, y: 2},
-	)
+	err := r.Fit(testDataPoints...)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	p, err := r.Predict(25)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Logf("Predict(4): %+v", p)
 	t.Logf("R-squared: %+v", r.GetR2())
+	evaluateRegression(t, r)
 }
---
+++ regressions_test.go
@@ -0,0 +1,28 @@
+package regressions
+
+import "testing"
+
+var testDataPoints = []DataPoint{
+	dataPoint{x: 6, y: 30},
+	dataPoint{x: 20, y: 12},
+	dataPoint{x: 26, y: 9},
+	dataPoint{x: 30, y: 8},
+	dataPoint{x: 37, y: 6},
+	dataPoint{x: 42, y: 5},
+	dataPoint{x: 47, y: 4},
+	dataPoint{x: 54, y: 3},
+	dataPoint{x: 64, y: 2},
+}
+
+func evaluateRegression(t *testing.T, r Regression) {
+	for _, dp := range testDataPoints {
+		x := dp.GetX()
+		observed := dp.GetY()
+		predicted, err := r.Predict(x)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		t.Logf("%v:\t%v (observed)\t%v (predicted)", x, observed, predicted)
+	}
+}
