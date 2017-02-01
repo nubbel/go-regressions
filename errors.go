50da7c0f87897e9e721fb6f158a6abb2d7d0c097
Dominique d’Argent <dominique.dargent@me.com>
2017-02-01 16:06:11 +0100

    Add Value method to logarithm error

--- errors.go
+++ errors.go
@@ -5,6 +5,12 @@ import "fmt"
 // ErrLogUndefined signifies that the logarithm of the underlying value is undefined.
 type ErrLogUndefined float64
 
+// Value returns the floating point value for which the logarithm is undefined.
+func (f ErrLogUndefined) Value() float64 {
+	return float64(f)
+}
+
+// Error implements the error interface
 func (f ErrLogUndefined) Error() string {
-	return fmt.Sprintf("Logarithm of %g is undefined.", float64(f))
+	return fmt.Sprintf("Logarithm of %g is undefined.", f.Value())
 }
