3506051d9948eb6d3c0c75d62fbf7e097c5da392
Dominique d’Argent <dominique.dargent@me.com>
2017-02-01 12:31:01 +0100

    Add test for another combination

--- combined_test.go
+++ combined_test.go
@@ -15,3 +15,18 @@ func TestMaxCombinedLogExp(t *testing.T) {
 	t.Logf("R-squared: %+v", r.GetR2())
 	evaluateRegression(t, r)
 }
+
+func TestMeanCombinedLogLogExp(t *testing.T) {
+	mean := func(x, y float64) float64 {
+		return (x + y) / 2
+	}
+
+	r := NewCombined(mean, NewLogLog(), NewExponential())
+	err := r.Fit(testDataPoints...)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("R-squared: %+v", r.GetR2())
+	evaluateRegression(t, r)
+}
